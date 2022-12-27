bbaccd4ff3ce3fde884f9d6389236b9df3e0335c
MGKalinin <1953115@gmail.com>
2022-12-27 20:53:50 +0400

    Update Speed Typing.py

--- Kick Start 2022/Round A 2022/Speed Typing/Speed Typing.py
+++ Kick Start 2022/Round A 2022/Speed Typing/Speed Typing.py
@@ -1,1 +1,25 @@
-#
+import sys
+sys.stdin = open('input_ Speed Typing.txt')
+
+t = int(input())
+for i in range(1, t + 1):
+	I = input()  #оригинал
+	P = input()  #продукт
+
+	N = len(I)
+	M = len(P)
+	ptrI = 0
+	ptrP = 0
+
+	while ptrI < N and ptrP < M:
+		if I[ptrI] == P[ptrP]:
+			ptrI += 1
+			ptrP += 1
+		else:
+			ptrP += 1
+	if ptrI == N:
+
+		print(f'Case #{i}: {M-N}')
+	else:
+		print(f'Case #{i}: IMPOSSIBLE')
+
