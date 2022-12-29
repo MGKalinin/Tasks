a1755bd68834cc9816764fdced3031b24b5d52a6
Попов Максим <1953115@gmail.com>
2022-12-29 21:48:21 +0300

    probe Ch Nine

--- Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
+++ Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
@@ -1,1 +1,22 @@
-#
+a = [3, 4]
+b = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+Al = len(a)  # длина a
+Bl = len(b)  # длина b
+Ai = 0  # индекс a
+Bi = 0  # индекс b
+r=[]
+# цикл по списку a, пока индекс a не больше индекс max a
+while Ai<Al:
+    for i in b:
+        if a[Ai]<b[Bi]:
+            a.insert(Ai,b[Bi])
+            print(a)
+        if sum(a)%9!=0:
+            continue
+        else:
+            r.append(a)
+            Ai+=1
+            Bi+=1
+
+# индекс a 0
+# если b индекс 0 больше a  индекс ноль - найти сумму а
