61c84b76b141e1dbd39afa8eda46168558b2a2ce
MGKalinin <1953115@gmail.com>
2022-12-30 09:48:38 +0000

    changed the file

--- Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
+++ Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
@@ -1,23 +1,16 @@
-a = [3, 4]
-b = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-Al = len(a)  # длина a
-Bl = len(b)  # длина b
-Ai = 0  # индекс a
-Bi = 0  # индекс b
-r=[]
-# цикл по списку a, пока индекс a не больше индекс max a
-while Ai<Al:
-    for i in b:
-        if a[Ai]<b[Bi]:
-            a.insert(Ai,b[Bi])
+"""Подстановка на 0 индекс исходного числа."""
+a=[2,3,4]
+b=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+aIndex=0
+bIndex=0
+rez_list=[]
+while aIndex<4:
+    if a[aIndex]<b[bIndex]:
+        a.insert(0,b[bIndex])
+        print(a)
+        if sum(a)%9==0:
+            print(sum(a))
             print(a)
-        if sum(a)%9!=0:
-            continue
-        else:
-            r.append(a)
-            Ai+=1
-            Bi+=1
-
-# индекс a 0
-# если b индекс 0 больше a  индекс ноль - найти сумму а
-# типа Codespase и 22
+            rez_list.append(a)
+            print(rez_list)
+            
