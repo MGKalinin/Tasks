d6a9ea0cd5a10b4818430c9f00924b944888dcd4
MGKalinin <1953115@gmail.com>
2022-12-30 13:13:07 +0000

    rere

--- Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
+++ Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
@@ -1,7 +1,11 @@
 n=234
-str_n=str(n)
-len_n=len(str_n)
-for i in range(str_n+1): #проход по длине исходного числа
+num=str(n)
+length=len(num)
+ans=[]
+for i in range(length+1): #проход по длине исходного числа
     for k in range(0,10): #проход по диапазону чисел от 10 до 10
         #первая цифра из добавленного числе не должна быть равна 0
+        if not k!=0 and i!=0:
         #вставить цифры в строку по индексу,привести к int и проверить % ==0
+            num = num[:i] + str(k) + num[i:]
+            print(num)
