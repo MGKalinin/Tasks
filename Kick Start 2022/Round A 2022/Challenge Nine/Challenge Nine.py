e6d4000698ed885e3a462fbd66bad2d56319e0e7
MGKalinin <1953115@gmail.com>
2022-12-30 12:54:16 +0000

    renovation

--- Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
+++ Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
@@ -1,21 +1,7 @@
-"""Подстановка на 0 индекс исходного числа."""
-a=[2,3,4]
-b=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-aIndex=0
-bIndex=0
-rez_list=[]
-while a:
-    if a[aIndex]<b[bIndex]:
-        a.insert(0,b[bIndex])
-        print(a)
-        if sum(a)%9==0:
-            print(sum(a))
-            print(a)
-            rez_list.append(a)
-            print(rez_list)
-            a.pop(0)
-            #aIndex+=1
-            bIndex+=1
-        continue
-        
-        
+n=234
+str_n=str(n)
+len_n=len(str_n)
+for i in range(str_n+1): #проход по длине исходного числа
+    for k in range(0,10): #проход по диапазону чисел от 10 до 10
+        #первая цифра из добавленного числе не должна быть равна 0
+        #вставить цифры в строку по индексу,привести к int и проверить % ==0
