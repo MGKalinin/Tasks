e725a34d9fb06c3f925ee8a7335f7d72d200860c
Попов Максим <1953115@gmail.com>
2023-01-04 18:02:09 +0300

    wrong answer...

--- Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
+++ Kick Start 2022/Round A 2022/Challenge Nine/Challenge Nine.py
@@ -2,26 +2,25 @@ import sys
 
 sys.stdin = open('input_ Challenge Nine.txt')
 
-
-t = int(input())
-# print(t)
-for i in range(1, t + 1):
-    N = input()
-    # print(N)
-    S = str(N)  # число в строку
-    L = len(S)  # длина строки
-    ans = []
-    for k in range(L + 1):
-        for z in range(0, 10):
-            if (k == 0 and z == 0) is False:
-                num = S[:k] + str(z) + S[k:]
-                if int(num) % 9 == 0:
-                    ans.append(int(num))
-                num = S
-    print(f'Case #{i}: {min(ans)}')
-
+# t = int(input())
+# for i in range(1, t + 1):
+#     N = input()
+#     # print(N)
+#     S = str(N)  # число в строку
+#     L = len(S)  # длина строки
+#     ans = []
+#     for k in range(L + 1):
+#         for z in range(0, 10):
+#             if (k == 0 and z == 0) is False:
+#                 num = S[:k] + str(z) + S[k:]
+#                 if int(num) % 9 == 0:
+#                     ans.append(int(num))
+#                 num = S
+#     print(f'Case #{i}: {min(ans)}')
 
 # time limit exceeded
+
+
 # def challenge_nine(n):
 #     num = str(n)
 #     l = len(num)
@@ -44,3 +43,13 @@ for i in range(1, t + 1):
 #     print(f'Case #{case}: {ans}')
 
 # time limit exceeded...
+
+
+t = int(input())
+s = 0
+for i in range(1, t + 1):
+    s += int(i)
+    r = int(t) * 10 + 9 - (s % 9)
+    print(f'Case #{i}: {r}')
+
+# wrong answer...
