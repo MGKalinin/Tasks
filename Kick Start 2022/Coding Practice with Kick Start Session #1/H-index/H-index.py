a01ee9f238500e9028ba9a6d6494ea78c79d273f
Попов Максим <1953115@gmail.com>
2023-01-06 23:45:33 +0300

    h_index

--- Kick Start 2022/Coding Practice with Kick Start Session #1/H-index/H-index.py
+++ Kick Start 2022/Coding Practice with Kick Start Session #1/H-index/H-index.py
@@ -1,15 +1,29 @@
-def h_index(n, citations):
-  ans = []
-  # TODO: Complete the function to get the H-Index scores after each paper
+import heapq
+import sys
 
-  return ans
+sys.stdin = open("input_H-index.txt")
+
+
+def h_index(n: int, citations: list):
+    minH = []
+    ans = []
+    hindex = 0
+    for i in range(n):
+        if citations[i] > hindex:
+            heapq.heappush(minH, citations[i])
+        while minH and minH[0] <= hindex:
+            heapq.heappop(minH)
+        if len(minH) >= hindex + 1:
+            hindex += 1
+        ans.append(hindex)
+    return ans
 
 
 if __name__ == '__main__':
-  t = int(input())
+    t = int(input())
 
-  for test_case in range(1, t + 1):
-    n = int(input())                      # The number of papers
-    citations = map(int, input().split()) # The number of citations for each paper
-    h_index_scores = h_index(n, citations)
-    print("Case #" + str(test_case) + ": " + ' '.join(map(str, h_index_scores)))
+    for test_case in range(1, t + 1):
+        n = int(input())  # The number of papers
+        citations = [int(s) for s in input().split()]  # The number of citations for each paper
+        h_index_scores = h_index(n, citations)
+        print("Case #" + str(test_case) + ": " + ' '.join(map(str, h_index_scores)))
