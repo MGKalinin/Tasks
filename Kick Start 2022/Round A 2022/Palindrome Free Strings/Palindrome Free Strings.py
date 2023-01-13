c02bd1e658a6109dc08c59d63ee94bd73ec538b6
maksimkalinin <1953115@gmail.com>
2023-01-13 23:56:07 +0300

    logic?

--- Kick Start 2022/Round A 2022/Palindrome Free Strings/Palindrome Free Strings.py
+++ Kick Start 2022/Round A 2022/Palindrome Free Strings/Palindrome Free Strings.py
@@ -2,13 +2,18 @@ import sys
 
 sys.stdin = open("input_Palindrome Free Strings.txt")  # для pycharm, тлф
 
-
 # sys.stdin=open('/workspaces/Tasks/Kick Start 2022/Round A 2022/Palindrome Free Strings/input_Palindrome Free
 # Strings.txt') #для Codespaces GitHub нужно скопировать  путь
 
+probe = ['1', '0', '0', '?', '?']
+
 
 def palindr(s):
-    return s == s[::-1]
+    ls_ch = [ind for ind, z in enumerate(s) if z == '?']
+    print(f'***{ls_ch}***')
+    # заменить на 0,проверить на палиндроме;
+
+    return True
 
 
 def d1000000():
@@ -28,3 +33,5 @@ def d1000000():
 
 for case in range(int(input())):
     print('Case #%d: %s' % (case + 1, d1000000()))
+
+print(palindr(probe))
