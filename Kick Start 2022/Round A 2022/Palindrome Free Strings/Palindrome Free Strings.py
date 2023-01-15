94b561ac6be36e9b45f208e9efcefc868d96db5a
maksimkalinin <1953115@gmail.com>
2023-01-16 00:55:32 +0300

    pass

--- Kick Start 2022/Round A 2022/Palindrome Free Strings/Palindrome Free Strings.py
+++ Kick Start 2022/Round A 2022/Palindrome Free Strings/Palindrome Free Strings.py
@@ -5,42 +5,34 @@ sys.stdin = open("input_Palindrome Free Strings.txt")  # для pycharm, тлф
 # sys.stdin=open('/workspaces/Tasks/Kick Start 2022/Round A 2022/Palindrome Free Strings/input_Palindrome Free
 # Strings.txt') #для Codespaces GitHub нужно скопировать  путь
 
-probe = ['1', '0', '0', '?', '?']
+def verification(s):
+    return len(s) <= 4 or s != s[::-1]
 
 
-def check_palindrome(h):
-    return h == h[::-1]
-
-
-def palindr(s):
-    for i, z in enumerate(s):
-        if z == '?':
+def palindrome(s):
+    key = {i for i, z in enumerate(s) if z == '?'}
+    # print(key, type(key))  # {3, 4, 5} <class 'set'>
+    i = 0
+    rez = []
+    while i < len(s):
+        if i in key:
             s[i] = '0'
-            if '?' not in s:
-                if check_palindrome(s):
-                    s[i] = '1'
-                    if check_palindrome(s):
-                        return False
+            rez.append(i)
+        while not (verification(s[i - 4:i + 1]) and
+                   verification(s[i - 5:i + 1])):
+            if not rez:
                 return False
+            i = rez.pop()
+            s[i] = '1'
+        i += 1
+    return True
 
 
-def verification():
-    N = int(input())  # len str
-    # print(N, type(N))  # int
-    S = [s for s in input().split(' ')]
-    # print(S)
-    line = [_ for _ in S[0]]  # str
-    print(f'это line{line}')
-    for j in range(N - 4):
-        new_line = line[j:j + 5]
-        print(f'это new_line{new_line}')
-        if palindr(new_line):  # вот здесь прописать
-            # переход к следующему отрезку
-            return 'POSSIBLE'
-        return 'IMPOSSIBLE'
+def palindrome_free_strings():
+    N = int(input().strip())
+    S = list(input().strip())
+    return "POSSIBLE" if palindrome(S) else "IMPOSSIBLE"
 
 
 for case in range(int(input())):
-    print('Case #%d: %s' % (case + 1, verification()))
-
-# print(palindr(probe))
+    print('Case #%d: %s' % (case + 1, palindrome_free_strings()))
