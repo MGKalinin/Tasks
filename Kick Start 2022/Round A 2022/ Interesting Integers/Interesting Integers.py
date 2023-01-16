96d4242efd557cf945f2c82c9c5f750afdaa2905
maksimkalinin <1953115@gmail.com>
2023-01-16 16:23:21 +0300

    add codewars

---
+++ Kick Start 2022/Round A 2022/ Interesting Integers/Interesting Integers.py
@@ -0,0 +1,5 @@
+# Let us call an integer interesting if the product of
+# its digits is divisible by the sum of its digits.
+# You are given two integers A and B.
+# Find the number of interesting integers between
+# A and B (both inclusive).
