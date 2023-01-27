668dcb98705e8e03a8e3a9e9f970b5f41f003ca9
MGKalinin <1953115@gmail.com>
2023-01-27 07:10:04 +0000

    return

--- Kick Start 2022/Round B 2022/ Palindromic Factors/Palindromic Factors.py
+++ Kick Start 2022/Round B 2022/ Palindromic Factors/Palindromic Factors.py
@@ -1,7 +1,7 @@
 import sys
 
 sys.stdin = open("/workspaces/Tasks/Kick Start 2022/Round B 2022/ Palindromic Factors/input_Palindromic Factors_ts1_.txt")
-
+#/workspaces/Tasks/Kick Start 2022/Round B 2022/ Palindromic Factors/input_Palindromic Factors_ts1_.txt
 
 def isPalindrome(a):
     s = str(a)
@@ -9,27 +9,17 @@ def isPalindrome(a):
     return s == rev
 
 
-def square_root(x):
-    return int(x ** 0.5)
-
-
 t = int(input())
-# Let a and b be two factors of A such that A=ab and a≤b.
-# Then a≤A−−√. It follows that we can find all factors of A
-# by checking the first A−−√ numbers only. For each factor a≤A−−√,
-# the number b=Aa≥A−−√ is also a factor of A.
 for i in range(1, t + 1):
-    A = int(input())
+    n = int(input())
+    # print(n, type(n))
     r = []
-    for a in range(1, square_root(A) + 1):
-        if A % a == 0:
-            if isPalindrome(a):
-                r.append(a)
-            b = A // a
-            if isPalindrome(b):
-                r.append(b)
+    for k in range(1, n + 1):
+        if n % k == 0:
+            if isPalindrome(k):
+                r.append(k)
+    rez = len(r)
 
-    # print(r)
-    rez = len(set(r))
-
-print(f'Case #{i}: {rez}')
+    print(f'Case #{i}: {rez}')
+# time limit exceeded
+# проверить до корень квадратный из числа
