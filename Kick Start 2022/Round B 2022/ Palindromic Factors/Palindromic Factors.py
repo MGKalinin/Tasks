ce49ffd672f03900c973239f37ee3407e395eeb3
maksimkalinin <1953115@gmail.com>
2023-01-28 08:41:06 +0300

    ts1

--- Kick Start 2022/Round B 2022/ Palindromic Factors/Palindromic Factors.py
+++ Kick Start 2022/Round B 2022/ Palindromic Factors/Palindromic Factors.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open("input_Palindromic Factors.txt")
+sys.stdin = open("input_Palindromic Factors_ts1_.txt")
 
 
 def isPalindrome(a):
@@ -11,26 +11,6 @@ def isPalindrome(a):
 
 def square_root(x):
     return int(x ** 0.5)
-
-
-# t = int(input())
-# for i in range(1, t + 1):
-#     n = int(input())
-#     p = square_root(n)  # квадрат n
-#     p_l = [x for x in range(square_root(n))]  # список от 0
-#     # до квадрата n
-#     print(f'это p_l {p_l}')
-#     r = []
-#     for k in range(1, n + 1):
-#         if n % k == 0:
-#             if isPalindrome(k):
-#                 r.append(k)
-#     print(f'это r {r}')
-#     rez = len(r)
-#
-#     print(f'Case #{i}: {rez}')
-# time limit exceeded
-# проверить до корень квадратный из числа
 
 
 t = int(input())
