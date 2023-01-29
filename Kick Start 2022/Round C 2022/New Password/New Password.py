7780f0fad5cb32a313eaa8a33e0c32403ce81163
maksimkalinin <1953115@gmail.com>
2023-01-30 02:07:55 +0300

    first step

--- Kick Start 2022/Round C 2022/New Password/New Password.py
+++ Kick Start 2022/Round C 2022/New Password/New Password.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.stdin = open("New Password.txt")
 
 # t number of test cases
@@ -12,16 +13,30 @@ sys.stdin = open("New Password.txt")
 # 4.At least one digit.
 # 5.At least one special character #, @, *, &.
 
+keys = ['#', '@', '*', '&']
+
+
+def spec_ch(a):
+    key = ['#', '@', '*', '&']
+    for j in key:
+        if j in a:
+            return True
+        return False
+
+
 t = int(input())
 for i in range(1, t + 1):
     N = [int(s) for s in input().split(' ')]
     P = [s for s in input().split(' ')]
-    print(N[0],type(N[0]))
-    print(P[0],type(P[0]))
-    # if not (any(x.islower() for x in text)):
-    for k in P[0]:
-        print(k)
+    print(N[0], type(N[0]))
+    print(P[0], type(P[0]))
+    if not (any(x.islower() for x in P)):
+        print('no1')
+    if not (any(x.isupper() for x in P)):
+        print('no2')
+    if not (any(x.isdigit() for x in P)):
+        print('no3')
+    print(spec_ch(P))
 
 
 # print(f'Case #{i}: {N},{P} ')
-
