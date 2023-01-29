79744ce1a2b1d6401bb14828db998c6c12f2532c
maksimkalinin <1953115@gmail.com>
2023-01-30 02:39:42 +0300

    add ps

--- Kick Start 2022/Round C 2022/New Password/New Password.py
+++ Kick Start 2022/Round C 2022/New Password/New Password.py
@@ -38,5 +38,30 @@ for i in range(1, t + 1):
         print('no3')
     print(spec_ch(P))
 
-
-# print(f'Case #{i}: {N},{P} ')
+# string createNewPassword(string oldPassword) {
+#     bool condition2 = false;
+#     bool condition3 = false;
+#     bool condition4 = false;
+#     bool condition5 = false;
+#     string newPassword = oldPassword;
+#     for (int i = 0; i < oldPassword.size(); i++) {
+#         if (oldPassword[i] >= 'A' && oldPassword[i] <= 'Z')
+#           condition2 = true;
+#         else if (oldPassword[i] >= 'a' && oldPassword[i] <= 'z')
+#           condition3 = true;
+#         else if (oldPassword[i] >= '0' && oldPassword[i] <= '9')
+#           condition4 = true;
+#         else if (oldPassword[i] == '@' || oldPassword[i] == '#' || oldPassword[i] == '&' || oldPassword[i] == '*')
+#           condition5 = true;
+#     }
+#
+#     if (!condition2) newPassword.append("A"); // Append any uppercase English alphabet letter.
+#     if (!condition3) newPassword.append("a"); // Append any lowercase English alphabet letter.
+#     if (!condition4) newPassword.append("1"); // Append any digit.
+#     if (!condition5) newPassword.append("#"); // Append any special character.
+#
+#     // Append any digit, letter, or a special character.
+#     while (newPassword.size() < 7) newPassword.append("1");
+#
+#     return newPassword;
+# }
